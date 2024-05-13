7584071a3742472abf479045b08250e29c369d05
RamtinKosari <ramtinkosari@gmail.com>
2024-05-13 09:56:33 +0330

    [#REFACTOR] Add Copyright

--- QSSL.hpp
+++ QSSL.hpp
@@ -3,6 +3,7 @@
      * @file QSSL.hpp
      * @author Ramtin Kosari (ramtinkosari@gmail.com)
      * @brief QSSL Open Source Astronomy and Astrophysics Library
+     * @copyright Copyright (c) 2023-2037, QSS Company - Section IRC (International Research Center)
      * @note Enjoy Using this Library
      * @date 2024-01-06
      */
