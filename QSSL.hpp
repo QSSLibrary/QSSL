61de6b53302168500aacee282cd6e715096e9f2e
RamtinKosari <ramtinkosari@gmail.com>
2024-07-24 13:29:59 +0330

    [#REFACTOR] Add Celestial Objects Module to QSSL.hpp

--- QSSL.hpp
+++ QSSL.hpp
@@ -8,5 +8,31 @@
      * @date 2024-01-06
      */
     # define QSSL
-    //-- Check QSSL Version
+    //-- Check if QAPB Built Library
+    # ifdef QAPB
+        //-- Check if QSSL License Configured
+        # ifndef HAS_QSSL_MODULE_LICENSE
+            # error QSSL License is Required for Celestial Objects
+        # endif // HAS_QSSL_MODULE_LICENSE
+        //-- Check if QSSL Version Configured
+        # ifndef HAS_QSSL_MODULE_VERSION
+            # error QSSL Version is Required for Celestial Objects
+        # endif // HAS_QSSL_MODULE_VERSION
+        //-- Check if QSSL Celestial Objects Configured
+        # ifndef HAS_QSSL_MODULE_CELESTIALOBJECTS
+            # error QSSL Celestial Objects is Required for QSSL
+        # endif // HAS_QSSL_MODULE_CELESTIAL_OBJECTS
+    # endif // QAPB
+    //-- Include QSSL License
+    # ifndef QSSL_LICENSE
+        # include "License.hpp"
+    # endif // QSSL_LICENSE
+    //-- Include QSSL Version
+    # ifndef QSSL_VERSION
+        # include "Version.hpp"
+    # endif // QSSL_VERSION
+    //-- Include QSSL Celestial Objects
+    # ifndef QSSL_CELESTIAL_OBJECTS
+        # include "CelestialObjects.hpp"
+    # endif // QSSL_CELESTIAL_OBJECTS
 # endif // QSSL
